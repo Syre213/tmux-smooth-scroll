9ce25da8b4a3f66fa19f8bf87ba3ccf4dca89d2f
Azor <azorng@gmail.com>
2025-12-17 01:26:13 +0200

    fix: prevent race condition (#2)

--- src/animate.sh
+++ src/animate.sh
@@ -11,10 +11,14 @@ BASE_DELAY_US=$3
 [[ ! "$DIRECTION" =~ ^(up|down)$ ]] && exit 1
 [[ ! "$LINES" =~ ^[0-9]+$ ]] || [ "$LINES" -lt 1 ] && exit 1
 
+# Execute animation
 perl -MTime::HiRes=usleep -e '
     my $delay = $ARGV[0];
-    for (my $i = 0; $i < $ARGV[1]; $i++) {
-        system("tmux", "send-keys", "-X", "scroll-" . $ARGV[2]);
-        usleep($delay) if $i < $ARGV[1] - 1;
+    my $lines = $ARGV[1];
+    my $direction = $ARGV[2];
+    
+    for (my $i = 0; $i < $lines; $i++) {
+        system("tmux", "send-keys", "-X", "scroll-" . $direction);
+        usleep($delay) if $i < $lines - 1;
     }
 ' "$BASE_DELAY_US" "$LINES" "$DIRECTION"
